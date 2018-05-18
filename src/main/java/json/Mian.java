6be57d8f06294a8c294f449c5c203275d1ebb245
LiuLi46 <18811355667@163.com>
2018-05-18 15:05:04 +0800

    ok

---
+++ src/main/java/json/Mian.java
@@ -0,0 +1,15 @@
+package json;
+
+/**  
+* <p>Title: mian</p>  
+* <p>Description: </p>  
+* @author liuli  
+* @date 2018Äê5ÔÂ18ÈÕ  
+*/
+public class Mian {
+
+  public static void main(String[] args) {
+    
+    System.out.println("you is dog");
+  }
+}
